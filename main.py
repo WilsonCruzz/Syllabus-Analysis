30ea2e9d456241847c372d9ebf066a935c0edf24
WilsonCruzz <wilson860731@gmail.com>
2024-03-16 00:35:48 -0400

    test

---
+++ main.py
@@ -0,0 +1,21 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+import openpyxl
+
+webUrl=urllib.request.urlopen("https://barrie.ca")
+html=webUrl.read()
+
+soup=BeautifulSoup(html,"html.parser")
+displayedText=soup.get_text()
+listOfText=displayedText.split()
+for word in listOfText:
+    digits='0123456789'
+    count=0
+    for i in range (len(word)):
+        if word[i] in digits:
+            count=count+1
+    if count==10:
+        print(word)
+    else:
+        pass
