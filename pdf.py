9b0f3f54ed8d14da636a5b89109a557911577b1e
WilsonCruzz <wilson860731@gmail.com>
2024-03-25 13:03:19 -0400

    up

---
+++ pdf.py
@@ -0,0 +1,83 @@
+import PyPDF2
+
+'''
+Target List
+'''
+
+dueDateList = [
+    "Deadline", "Hand in",
+    "Submission", "Turn in",
+    "D-day", "Deliverable",
+    "Task Date"
+]
+
+gradesList = [
+    "Grades", "Marks",
+    "Score", "Percentage", "Weightage",
+    "Evaluation", "Performance"
+]
+
+assignmentsList = [
+    "Assignment", "Homework",
+    "Project", "Task", "Exercise",
+    "Problem Set", "Report"
+]
+
+examsList = [
+    "Exam", "Quiz",
+    "Test", "Mid-term",
+    "Final", "Assessment"
+]
+'''
+weekList
+'''
+weekList = ['week1', 'week2', 'week3', 'week4', 'week5', 'week6',
+            'week7', 'study week', 'week8', 'week9', 'week10',
+            'week11', 'week12', 'week13', 'week14']
+tableContentList = []
+
+targetList = dueDateList + gradesList + assignmentsList + examsList
+catchList = []
+# pdf
+pdfFileObj = open(r"C:\Users\wilso\Desktop\Syllabus-COMP1054-2024W.pdf", 'rb')
+pdfReader = PyPDF2.PdfReader(pdfFileObj)
+
+numContextWords = 10
+pagesWithKeywords = {}
+
+for pageNum, page in enumerate(pdfReader.pages):
+    pageText = page.extract_text()
+    words = pageText.split()
+    for target in targetList:
+        if target in words:
+            targetIndex = words.index(target)
+
+            startIndex = max(0, targetIndex - numContextWords)
+            endIndex = min(len(words), targetIndex + numContextWords + 1)
+            context = ''
+            for i in range(startIndex, endIndex):
+                context += words[i] + ' '
+            if pageNum in pagesWithKeywords:
+                pagesWithKeywords[pageNum].append((target, context))
+            else:
+                pagesWithKeywords[pageNum] = [(target, context)]
+            print(target, context)
+
+pdfFileObj.close()
+
+'''
+print(len(pdfReader.pages))
+page0Obj = pdfReader.pages[0]
+page0Text = page0Obj.extract_text()
+page1Obj = pdfReader.pages[1]
+page1Text = page1Obj.extract_text()
+page2Obj = pdfReader.pages[2]
+page2Text = page2Obj.extract_text()
+page3Obj = pdfReader.pages[3]
+page3Text = page3Obj.extract_text()
+
+print(page0Text)
+print(page1Text)
+print(page2Text)
+print(page3Text)
+'''
