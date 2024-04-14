946dc90e1ce5313d0b63ba62af9f928734f46dd2
WilsonCruzz <wilson860731@gmail.com>
2024-04-13 22:55:34 -0400

    Remove pdf.py

--- constants/targetList.py
+++ constants/targetList.py
@@ -1,6 +1,13 @@
-'''
-Target List
-'''
+"""
+The targetList is a list that combines several other lists. These lists include:
+
+1. dueDateList: Contains phrases related to due dates.
+2. gradesList: Contains percentages from 1% to 100%. These represent possible grades or weights for assignments.
+3. assignmentsList: Contains phrases related to assignments.
+4. examsList: Contains phrases related to exams.
+
+The targetList is used to filter words in a document. Any word that matches an item in the targetList is considered a "target word". These target words are used for further processing, such as generating a weekly schedule for students.
+"""
 dueDateList = [
     "deadline ", "hand in ",
     "submission ", "turn in",
@@ -35,9 +42,8 @@ examsList = [
 '''
 weekList
 '''
-weekList = ['week 1', 'Week 1', 'week 2', 'Week 2', 'week 3', 'Week 3', 'week 4', 'Week 4', 'week 5', 'Week 5',
-            'week 6', 'Week 6', 'week 7', 'Week 7', 'week 8', 'Week 8', 'week 9', 'Week 9', 'week 10', 'Week 10',
-            'week 11', 'Week 11', 'week 12', 'Week 12', 'week 13', 'Week 13', 'week 14', 'Week 14', 'week 15', 'Week 15'
-    , 'study week']
+weekList = ['week 1', 'week 2', 'week 3', 'week 4', 'week 5',
+            'week 6', 'week 7', 'week 8', 'week 9', 'week 10',
+            'week 11', 'week 12', 'week 13', 'week 14', 'week 15', 'study week']
 
 targetList = dueDateList + gradesList + assignmentsList + examsList
